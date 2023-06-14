f7cb7ee37d4dce96a3465262365cd855d791b7a4
Oleksandr Levchuk <73852716+alxlvgit@users.noreply.github.com>
2023-06-13 20:17:18 -0700

    update the Projects section

--- data.js
+++ data.js
@@ -1,53 +1,153 @@
-const swiperData = {
-    "all": [{
-        "projectTitle": "BillTracker",
-        "path": "resources/images/android-bills.png",
-        "gitHubLink": "https://github.com/alxlvgit/BillTracker"
+const data = {
+  all: [
+    {
+      projectTitle: "TravelMeet",
+      thumbnail: "resources/images/travelmeet_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/TravelMeet",
+      hostedLink: "https://web-production-fa4f.up.railway.app/",
+      tech: "JavaScript, Node.js, Express, Socket.io, MySQL, HTML, Tailwind CSS, EJS, Redis, AWS S3, Passport.js",
+      description:
+        "TravelMeet is a web app that helps users connect with new people and discover events in specific locations. It features a dynamic map that provides real-time shared location information of users and events in the selected area. Users can view profiles, follow others, and create groups to plan event visits together.",
+      images: [
+        "resources/images/travelmeet_1.png",
+        "resources/images/travelmeet_2.png",
+        "resources/images/travelmeet_3.png",
+        "resources/images/travelmeet_4.png",
+      ],
     },
     {
-        "projectTitle": "BluetoothTimer",
-        "path": "resources/images/android-bluetooth.png",
-        "gitHubLink": "https://github.com/alxlvgit/BluetoothTimer"
+      projectTitle: "Personal Website",
+      thumbnail: "resources/images/personal_portfolio_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/PersonalWebsite",
+      hostedLink: "https://levchuk.dev/",
+      tech: "HTML, CSS, JavaScript",
+      description:
+        "Personal Website is a website that showcases my projects and skills. The website is built using HTML, CSS, and JavaScript.",
+      images: ["resources/images/personal_website.png"],
     },
     {
-        "projectTitle": "Real-time photo feed",
-        "path": "resources/images/real_time_app.png",
-        "gitHubLink": "https://github.com/alxlvgit/realTimePhotoFeed"
+      projectTitle: "Real-time photo feed",
+      thumbnail: "resources/images/real_time_app_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/realTimePhotoFeed",
+      tech: "Node.js, Socket.io, HTML, CSS, EJS",
+      description:
+        "Real-time photo feed is a web application that allows users to upload photos and view photos uploaded by other users in real-time. The application is built using Node HTTP module and Socket.io.",
+      images: ["resources/images/real_time_app.png"],
     },
     {
-        "projectTitle": "Grayscale filter",
-        "path": "resources/images/grayscale_project.png",
-        "gitHubLink": "https://github.com/alxlvgit/grayscalePhoto"
+      projectTitle: "Grayscale filter",
+      thumbnail: "resources/images/grayscale_project_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/grayscalePhoto",
+      tech: "Node.js",
+      description:
+        "Grayscale filter is a web application that allows users to unzip the given file with photos, and apply a grayscale filter to the images. The application is built using Node.js.",
+      images: ["resources/images/grayscale_project.png"],
     },
     {
-        "projectTitle": "Personal Website",
-        "path": "resources/images/personal_website.png",
-        "gitHubLink": "https://github.com/alxlvgit/PersonalWebsite"
-    }],
-    "front-end": [{
-        "projectTitle": "Personal Website",
-        "path": "resources/images/personal_website.png",
-        "gitHubLink": "https://github.com/alxlvgit/PersonalWebsite"
-    }],
-    "back-end": [{
-        "projectTitle": "Real-time photo feed",
-        "path": "resources/images/real_time_app.png",
-        "gitHubLink": "https://github.com/alxlvgit/realTimePhotoFeed"
+      projectTitle: "BluetoothTimer",
+      thumbnail: "resources/images/bluetooth_timer_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/BluetoothTimer",
+      tech: "Java, Android Studio",
+      description:
+        "BluetoothTimer is an Android app developed using Java and Android Studio. Its main functionality is to allow users to set a timer to automatically disable the Bluetooth adapter on their device. This app provides convenience by automating the Bluetooth disabling process.",
+      images: ["resources/images/bluetooth_timer.png"],
     },
     {
-        "projectTitle": "Grayscale filter",
-        "path": "resources/images/grayscale_project.png",
-        "gitHubLink": "https://github.com/alxlvgit/grayscalePhoto"
-    }],
-    "android": [{
-        "projectTitle": "BillTracker",
-        "path": "resources/images/android-bills.png",
-        "gitHubLink": "https://github.com/alxlvgit/BillTracker"
+      projectTitle: "BillTracker",
+      thumbnail: "resources/images/android_bills_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/BillTracker",
+      tech: "Java, Android Studio",
+      description:
+        "BillTracker is an Android app developed using Java and Android Studio. Its purpose is to help users easily keep track of their bills. Users can add bills, create new categories, and view their bills either in a list format or visually represented on a pie chart.",
+      images: [
+        "resources/images/bill_tracker_1.png",
+        "resources/images/bill_tracker_2.png",
+      ],
+    },
+  ],
+  frontend: [
+    {
+      projectTitle: "TravelMeet",
+      thumbnail: "resources/images/travelmeet_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/TravelMeet",
+      hostedLink: "https://web-production-fa4f.up.railway.app/",
+      tech: "JavaScript, Node.js, Express, Socket.io, MySQL, HTML, Tailwind CSS, EJS, Redis, AWS S3, Passport.js",
+      description:
+        "TravelMeet is a web app that helps users connect with new people and discover events in specific locations. It features a dynamic map that provides real-time shared location information of users and events in the selected area. Users can view profiles, follow others, and create groups to plan event visits together.",
+      images: [
+        "resources/images/travelmeet_1.png",
+        "resources/images/travelmeet_2.png",
+        "resources/images/travelmeet_3.png",
+        "resources/images/travelmeet_4.png",
+      ],
     },
     {
-        "projectTitle": "BluetoothTimer",
-        "path": "resources/images/android-bluetooth.png",
-        "gitHubLink": "https://github.com/alxlvgit/BluetoothTimer"
-    }]
-}
-
+      projectTitle: "Personal Website",
+      thumbnail: "resources/images/personal_portfolio_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/PersonalWebsite",
+      hostedLink: "https://levchuk.dev/",
+      tech: "HTML, CSS, JavaScript",
+      description:
+        "Personal Website is a website that showcases my projects and skills. The website is built using HTML, CSS, and JavaScript.",
+      images: ["resources/images/personal_website.png"],
+    },
+  ],
+  backend: [
+    {
+      projectTitle: "TravelMeet",
+      thumbnail: "resources/images/travelmeet_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/TravelMeet",
+      hostedLink: "https://web-production-fa4f.up.railway.app/",
+      tech: "JavaScript, Node.js, Express, Socket.io, MySQL, HTML, Tailwind CSS, EJS, Redis, AWS S3, Passport.js",
+      description:
+        "TravelMeet is a web app that helps users connect with new people and discover events in specific locations. It features a dynamic map that provides real-time shared location information of users and events in the selected area. Users can view profiles, follow others, and create groups to plan event visits together.",
+      images: [
+        "resources/images/travelmeet_1.png",
+        "resources/images/travelmeet_2.png",
+        "resources/images/travelmeet_3.png",
+        "resources/images/travelmeet_4.png",
+      ],
+    },
+    {
+      projectTitle: "Real-time photo feed",
+      thumbnail: "resources/images/real_time_app_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/realTimePhotoFeed",
+      tech: "Node.js, Socket.io, HTML, CSS, EJS",
+      description:
+        "Real-time photo feed is a web application that allows users to upload photos and view photos uploaded by other users in real-time. The application is built using Node HTTP module and Socket.io.",
+      images: ["resources/images/real_time_app.png"],
+    },
+    {
+      projectTitle: "Grayscale filter",
+      thumbnail: "resources/images/grayscale_project_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/grayscalePhoto",
+      tech: "Node.js",
+      description:
+        "Grayscale filter is a web application that allows users to unzip the given file with photos, and apply a grayscale filter to the images. The application is built using Node.js.",
+      images: ["resources/images/grayscale_project.png"],
+    },
+  ],
+  android: [
+    {
+      projectTitle: "BillTracker",
+      thumbnail: "resources/images/android_bills_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/BillTracker",
+      tech: "Java, Android Studio",
+      description:
+        "BillTracker is an Android app developed using Java and Android Studio. Its purpose is to help users easily keep track of their bills. Users can add bills, create new categories, and view their bills either in a list format or visually represented on a pie chart.",
+      images: [
+        "resources/images/bill_tracker_1.png",
+        "resources/images/bill_tracker_2.png",
+      ],
+    },
+    {
+      projectTitle: "BluetoothTimer",
+      thumbnail: "resources/images/bluetooth_timer_thumbnail.png",
+      gitHubLink: "https://github.com/alxlvgit/BluetoothTimer",
+      tech: "Java, Android Studio",
+      description:
+        "BluetoothTimer is an Android app developed using Java and Android Studio. Its main functionality is to allow users to set a timer to automatically disable the Bluetooth adapter on their device. This app provides convenience by automating the Bluetooth disabling process.",
+      images: ["resources/images/bluetooth_timer.png"],
+    },
+  ],
+};
--- main.js
+++ main.js
@@ -1,79 +1,245 @@
 // Target filter buttons, default filter button, swiper navigation
-const buttons = document.querySelectorAll('button.filter-button');
+const buttons = document.querySelectorAll("button.filter-button");
 const defaultBtn = document.getElementById("all");
-const swiperNavigation = document.querySelectorAll(".swiper-nav");
 const contactBtn = document.querySelector(".contact-btn");
-
-// Initialize swiper
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    breakpoints: {
-        600: {
-            allowTouchMove: false
-        }
-    }
-});
+const projectsContainer = document.querySelector(".projects");
 
 // Set the default filter button active on window load
 window.addEventListener("load", () => {
-    defaultBtn.classList.add("active");
-    defaultBtn.click();
+  defaultBtn.classList.add("active");
+  defaultBtn.click();
 });
 
-// Hide Swiper navigation if only one slide available
-const hideSwiperNavButtons = (hideButtons) => {
-    swiperNavigation.forEach(button => {
-        if (hideButtons) {
-            button.classList.add("swiper-button-hidden");
-        } else {
-            button.classList.remove("swiper-button-hidden");
-        }
-    });
-}
+// Create project tech for overlay
+const createTech = (tech) => {
+  const projectTech = document.createElement("p");
+  projectTech.classList.add("project-tech");
+  projectTech.textContent = tech;
+  return projectTech;
+};
 
-// Add slides to Swiper based on the data set 
-const addSlides = (dataSetId) => {
-    swiper.removeAllSlides();
-    hideSwiperNavButtons(false);
-    const swiperDataSet = swiperData[dataSetId];
-    if (swiperDataSet.length < 2) {
-        hideSwiperNavButtons(true);
-    }
-    swiperDataSet.forEach((slideData, index) => {
-        swiper.addSlide(index, `<div class="swiper-slide">
-        <img src = "${slideData.path}">
-       <a class="github-swiper" href="${slideData.gitHubLink}">
-       <h2 class="project-title">${slideData.projectTitle}</h2>
-       <span class="github"></span>
-       </a>
-        </div>`);
-    });
-    swiper.slideTo(1, 0, false);
-}
+// Create info button for overlay
+const createInfoBtn = (project) => {
+  const infoBtn = document.createElement("button");
+  infoBtn.classList.add("info-button");
+  infoBtn.textContent = "MORE INFO";
+  infoBtn.addEventListener("click", () => {
+    renderProjectModal(project);
+  });
+  return infoBtn;
+};
 
-// Populate filtered slides in Swiper on button click
-buttons.forEach(function (filterBtn) {
-    filterBtn.addEventListener('click', () => {
-        const dataSetId = filterBtn.dataset.id;
-        const currentlyClickedBtn = document.getElementById(dataSetId);
-        document.querySelector(".active").classList.remove("active");
-        currentlyClickedBtn.classList.add("active");
-        addSlides(dataSetId);
-    });
-});
+// Create overlay handler
+const overlayHandler = (event, overlay) => {
+  if (event.currentTarget.classList.contains("overlay-active")) {
+    event.currentTarget.classList.remove("overlay-active");
+    return;
+  }
+  const previouslyActivatedOverlay = document.querySelector(".overlay-active");
+  previouslyActivatedOverlay
+    ? previouslyActivatedOverlay.classList.remove("overlay-active")
+    : null;
+  event.preventDefault();
+  event.stopPropagation();
+  overlay.classList.add("overlay-active");
+};
+
+// Create overlay for project tiles
+const createProjectOverlayOnHover = (project) => {
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  const tileTop = document.createElement("div");
+  tileTop.classList.add("tile-top");
+  const projectTitle = document.createElement("h2");
+  projectTitle.classList.add("title");
+  projectTitle.textContent = project.projectTitle;
+  tileTop.appendChild(projectTitle);
+  tileTop.appendChild(createTech(project.tech));
+  overlay.appendChild(tileTop);
+  overlay.appendChild(createInfoBtn(project));
+  overlay.addEventListener("touchstart", (event) => {
+    event.target.classList.contains("info-button")
+      ? null
+      : event.preventDefault();
+    overlayHandler(event, overlay);
+  });
+  overlay.addEventListener("mouseover", (event) => {
+    overlayHandler(event, overlay);
+  });
+  overlay.addEventListener("mouseout", () => {
+    overlay.classList.remove("overlay-active");
+  });
+  return overlay;
+};
+
+// Render project tiles in projects container
+const addProjectsTiles = (dataSetId) => {
+  projectsContainer.innerHTML = "";
+  data[`${dataSetId}`].map((project) => {
+    const projectTile = document.createElement("div");
+    projectTile.classList.add("project-tile");
+    projectTile.style.backgroundImage = `url(${project.thumbnail})`;
+    projectTile.appendChild(createProjectOverlayOnHover(project));
+    projectsContainer.appendChild(projectTile);
+  });
+};
+
+// Create close button for modal
+const createCloseBtn = (modal) => {
+  const closeBtn = document.createElement("span");
+  closeBtn.classList.add("close-btn");
+  closeBtn.innerHTML = "&times;";
+  closeBtn.addEventListener("click", () => {
+    modal.remove();
+  });
+  return closeBtn;
+};
+
+// Create project description for modal
+const createDescription = (description) => {
+  const projectDescription = document.createElement("p");
+  projectDescription.classList.add("project-description");
+  projectDescription.textContent = description;
+  return projectDescription;
+};
+
+// Create project link for modal
+const createProjectLink = (link) => {
+  const projectLink = document.createElement("a");
+  projectLink.classList.add("project-link");
+  projectLink.href = link;
+  projectLink.target = "_blank";
+  projectLink.innerHTML =
+    ' View Website <i class="fas fa-external-link-alt"></i>';
+  return projectLink;
+};
+
+// Create project GitHub link for modal
+const createGitHubLink = (link) => {
+  const gitHubLink = document.createElement("a");
+  gitHubLink.classList.add("github-link-modal");
+  gitHubLink.href = link;
+  gitHubLink.target = "_blank";
+  return gitHubLink;
+};
+
+// Create project title for modal
+const createTitle = (title) => {
+  const projectTitle = document.createElement("h2");
+  projectTitle.classList.add("project-title-modal");
+  projectTitle.textContent = title;
+  return projectTitle;
+};
+
+// Create modal overlay
+const createModalOverlay = (modal) => {
+  const modalOverlay = document.createElement("div");
+  modalOverlay.classList.add("modal-overlay");
+  modalOverlay.addEventListener("click", () => {
+    modal.remove();
+  });
+  return modalOverlay;
+};
+
+// Initialize swiper
+const initSwiper = () => {
+  new Swiper(".swiper", {
+    slidesPerView: "auto",
+    mousewheel: {
+      forceToAxis: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    speed: 800,
+  });
+};
+
+// Add swiper navigation
+const addSwiperNavigation = () => {
+  const swiperNavigation = document.createElement("div");
+  swiperNavigation.classList.add("swiper-navigation");
+  const swiperPrev = document.createElement("div");
+  swiperPrev.classList.add("swiper-button-prev", "swiper-navigation-button");
+  const swiperNext = document.createElement("div");
+  swiperNext.classList.add("swiper-button-next", "swiper-navigation-button");
+  swiperNavigation.appendChild(swiperPrev);
+  swiperNavigation.appendChild(swiperNext);
+  return swiperNavigation;
+};
+
+// Create project slides for modal
+const createSwiperComponents = (images) => {
+  const swiperContainer = document.createElement("div");
+  swiperContainer.classList.add("swiper");
+  const swiperWrapper = document.createElement("div");
+  const pagination = document.createElement("div");
+  pagination.classList.add("swiper-pagination");
+  swiperWrapper.classList.add("swiper-wrapper");
+  swiperContainer.appendChild(pagination);
+  swiperContainer.appendChild(addSwiperNavigation());
+  images.forEach((image) => {
+    const swiperSlide = document.createElement("div");
+    swiperSlide.classList.add("swiper-slide");
+    const swiperImage = document.createElement("img");
+    swiperImage.src = image;
+    swiperImage.alt = "Project image";
+    swiperSlide.appendChild(swiperImage);
+    swiperWrapper.appendChild(swiperSlide);
+  });
+  swiperContainer.appendChild(swiperWrapper);
+  return swiperContainer;
+};
+
+// Render project modal
+const renderProjectModal = (project) => {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("modal-content");
+  const projectInfoContainer = document.createElement("div");
+  projectInfoContainer.classList.add("project-info-container");
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("title-container");
+  titleContainer.appendChild(createTitle(project.projectTitle));
+  titleContainer.appendChild(createGitHubLink(project.gitHubLink));
+  const separator = document.createElement("hr");
+  separator.classList.add("separator");
+  projectInfoContainer.appendChild(titleContainer);
+  projectInfoContainer.appendChild(separator);
+  projectInfoContainer.appendChild(createDescription(project.description));
+  project.hostedLink
+    ? projectInfoContainer.appendChild(createProjectLink(project.hostedLink))
+    : null;
+  modalContent.appendChild(createSwiperComponents(project.images));
+  modalContent.appendChild(projectInfoContainer);
+  modalContent.appendChild(createCloseBtn(modal));
+  modal.appendChild(modalContent);
+  modal.appendChild(createModalOverlay(modal));
+  document.body.appendChild(modal);
+  initSwiper();
+};
 
 // Scroll down to contact section on button click
 contactBtn.addEventListener("click", () => {
-    const contactSection = document.getElementById("contact");
-    contactSection.scrollIntoView({
-        behavior: "smooth"
-    })
-})
+  const contactSection = document.getElementById("contact");
+  contactSection.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// Render project tiles on filter button click
+buttons.forEach(function (filterBtn) {
+  filterBtn.addEventListener("click", () => {
+    const dataSetId = filterBtn.dataset.id;
+    const currentlyClickedBtn = document.getElementById(dataSetId);
+    document.querySelector(".active").classList.remove("active");
+    currentlyClickedBtn.classList.add("active");
+    addProjectsTiles(dataSetId);
+  });
+});
